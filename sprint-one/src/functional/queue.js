2b872a8d2eb661f5d2e96206b640acdc62e61d56
Hack Reactor Students <github@hackreactor.com>
2013-09-25 16:07:38 -0700

    Fixes queue size

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,15 +5,18 @@ var makeQueue = function(){
   var storage = {};
   var size = 0;
   var first = 0;
+  var last = 0;
   var temp;
   // Implement the methods below
 
   instance.enqueue = function(value){
-    storage[size] = value;
+    storage[last] = value;
+    last++;
     size++;
   };
 
   instance.dequeue = function(){
+    size && size--;
     temp = storage[first];
     first++;
     return temp;
