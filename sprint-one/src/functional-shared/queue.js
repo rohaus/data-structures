084543a749c3312cc7544ac519113f5004c3e2f2
Hack Reactor Students <github@hackreactor.com>
2013-09-25 20:53:22 -0700

    Adds queue from functional class pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,33 @@
 var makeQueue = function(){
   // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+  var size = 0;
+  var first = 0;
+  var last = 0;
+  var temp;
+  // Implement the methods below
+
+  instance.enqueue = function(value){
+    storage[last] = value;
+    last++;
+    size++;
+  };
+
+  instance.dequeue = function(){
+    size && size--;
+    temp = storage[first];
+    first++;
+    return temp;
+  };
+
+  instance.size = function(){
+    return size;
+  };
+
+  return instance;
 };
 
 var queueMethods = {};
