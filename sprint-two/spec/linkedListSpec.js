211869a7bc1a0df7b5516d797c3d38ccc407e25f
Hack Reactor Students <github@hackreactor.com>
2013-09-26 11:29:02 -0700

    Adds test for linked list

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -17,4 +17,36 @@ describe("linkedList", function() {
   });
 
   // add more tests here to test the functionality of linkedList
+  it("should return the default value at the head when there is no head", function(){
+    expect(linkedList.head).toEqual(null);
+  });
+
+  it("should take a value and add it to the end of the list", function() {
+    linkedList.addToTail(1);
+    expect(linkedList.tail).toEqual(0);
+  });
+  
+  it("should return the value at the head after adding twice to the tail", function(){
+    linkedList.addToTail(3);
+    linkedList.addToTail(2);
+    expect(linkedList.head).toEqual(0);
+  });
+  
+  it("should add a value to the end of the list and be able to remove the first value", function(){
+    linkedList.addToTail(1);
+    expect(linkedList.removeHead()).toEqual(1);
+  });
+
+  it("should add two values consequtively and the remove the head", function(){
+    linkedList.addToTail(1);
+    linkedList.addToTail(2);
+    expect(linkedList.removeHead()).toEqual(1);
+  });
+
+  it("should return the value at the head after adding twice and removing once", function(){
+    linkedList.addToTail(1);
+    linkedList.addToTail(2);
+    linkedList.removeHead();
+    expect(linkedList.head).toEqual(1);
+  });
 });
