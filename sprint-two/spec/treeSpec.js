6a5ef5f6ad804895a47154f1e7a77c5701a09fd7
Hack Reactor Students <github@hackreactor.com>
2013-09-26 16:28:12 -0700

    Adds addChild function to tree.js and tests to treeSpecs.js

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -11,5 +11,32 @@ describe("tree", function() {
     expect('value' in tree).toBe(true);
   });
 
+  describe('addChild', function(){
+    it("should add a child tree to the parent tree", function(){
+      tree.addChild("ken");
+      expect(tree.children[0].value).toEqual("ken");
+    });
+  });
+
   // Add more tests here to test the functionality of tree.
+  describe('contains', function(){
+    it("should determine if a single tree contains a value",function(){
+      tree.addChild("ken");
+      expect(tree.contains("ken")).toEqual(true);
+    });
+
+    it("should determine if a value is in the entire tree", function(){
+      tree.addChild("ken");
+      tree.addChild("rohaus");
+      tree.addChild("marcus");
+      expect(tree.contains("rohaus")).toEqual(true);
+    });
+
+    it("should determine if a value is not in the entire tree", function(){
+      tree.addChild("ken");
+      tree.addChild("rohaus");
+      tree.addChild("marcus");
+      expect(tree.contains("lee")).toEqual(false);
+    });
+  });
 });
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,15 +1,27 @@
-var makeTree = function(){
+var makeTree = function(tree){
   var newTree = {};
-  newTree.value = undefined;
+  newTree.value = tree;
   newTree.children = undefined;
-
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(){
+treeMethods.addChild = function(child){
+  this.children = [];
+  this.children.push(makeTree(child));
 };
 
 treeMethods.contains = function(){
+
 };
+
+
+// tree = makeTree(); // {value: tree, children: undefined, contains: function(){} }
+// tree.addChild("ken"); // {value: tree, children: [{value: tree, children: "ken", contains: function(){}}], contains: function(){} }
+// tree.addChild("rohaus"); //
+
+   //  O
+   // o o
