e91e9d9b641ccbcf539626d507f435e093c3ec39
Hack Reactor Students <github@hackreactor.com>
2013-09-25 21:49:50 -0700

    Passes pseudoclassical class pattern tests

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,4 +1,18 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  this.storage = {};
+  this.count = 0;
 };
 
+Stack.prototype.push = function(value){
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+Stack.prototype.pop = function(){
+  this.count && this.count--; 
+  return this.storage[this.count];
+};
+
+Stack.prototype.size = function(){
+  return this.count;
+};
