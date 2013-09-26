02e1b48105408733e7b94304f0f7ac69c5e88b31
Hack Reactor Students <github@hackreactor.com>
2013-09-26 11:29:30 -0700

    Adds addToTail function

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,10 +1,17 @@
 // Note: don't use an array to do this.
 var makeLinkedList = function(){
   var list = {};
-  list.head = null;
-  list.tail = null;
+  list.head = null; //first
+  list.tail = null; //last
+  list.makeNode = makeNode;
 
-  list.addToTail = function(){
+  list.addToTail = function(value){
+    // list.makeNtail = node;
+    if (list.head === null){
+      list.head++;
+    }
+    list.tail++;
+    list[list.tail] = list.makeNode(value);
   };
 
   list.removeHead = function(){
@@ -23,3 +30,12 @@ var makeNode = function(value){
 
   return node;
 };
+
+// var linkedList = makeLinkedList() //{head: null, tail: null, makeNode: node, addToTail: fn.., removeHead: fn.., contains: fn...}
+// linkedList.addToTail(3); // {head: 3, tail: null, makeNode: node, addToTail: fn.., removeHead: fn.., contains: fn...}
+// makeNode(value);
+
+// linkedList = {
+//   0: {value: value, next: 1},
+//   1: {value: value, next: next}
+// }
