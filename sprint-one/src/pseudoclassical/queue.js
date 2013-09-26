5c40c310c4464ebaeaffe3357f0de246378199f8
Hack Reactor Students <github@hackreactor.com>
2013-09-25 21:54:57 -0700

    Passes pseudoclassical class pattern tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,4 +1,24 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this.storage = {};
+  this.count = 0;
+  this.first = 0;
+  this.last = 0;
+  this.temp = null;
 };
 
+Queue.prototype.enqueue = function(value){
+  this.storage[this.last] = value;
+  this.last++;
+  this.count++;
+};
+
+Queue.prototype.dequeue = function(){
+  this.count && this.count--;
+  this.temp = this.storage[this.first];
+  this.first++;
+  return this.temp;
+};
+
+Queue.prototype.size = function(){
+  return this.count;
+};
