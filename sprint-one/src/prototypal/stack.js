6759c6259769cfc8708cd15f6d3d8c5a39208024
Hack Reactor Students <github@hackreactor.com>
2013-09-25 21:37:55 -0700

    Passes prototypal class pattern tests

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,6 +1,24 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var instance = Object.create(stackMethods);
+
+  instance.storage = {};
+  instance.count = 0;
+
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.storage[this.count] = value;
+    this.count++;
+  },
 
+  pop: function(){
+    this.count && this.count--; 
+    return this.storage[this.count];
+  },
+
+  size: function(){
+    return this.count;
+  }
+};
