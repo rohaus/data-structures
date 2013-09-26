74480428944096620c16942ab4c67df21602bf62
Hack Reactor Students <github@hackreactor.com>
2013-09-25 21:41:00 -0700

    Passes prototypal class pattern tests

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,6 +1,30 @@
-var makeQueue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+var makeQueue = function(){
+  var instance = Object.create(queueMethods);
+  instance.storage = {};
+  instance.count = 0;
+  instance.first = 0;
+  instance.last = 0;
+  instance.temp = null;
+
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this.storage[this.last] = value;
+    this.last++;
+    this.count++;
+  },
+
+  dequeue: function(){
+    this.count && this.count--;
+    this.temp = this.storage[this.first];
+    this.first++;
+    return this.temp;
+  },
+
+  size: function(){
+    return this.count;
+  }
+};
 
