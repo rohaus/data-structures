9eb391395d8f4d70242289657c9598d19542ad96
Hack Reactor Students <github@hackreactor.com>
2013-09-27 11:32:29 -0700

    Adds hashTable tests and hashTable retrieve and hashTable set

--- sprint-two/spec/hashTableSpec.js
+++ sprint-two/spec/hashTableSpec.js
@@ -3,22 +3,56 @@ describe("hashTable", function() {
 
   beforeEach(function() {
     hashTable = new HashTable();
+    // console.log("the hashTable is: ", hashTable);
+    // console.log("Is the hashTable an array? "+Array.isArray(hashTable));
+    // console.log("the hashTable._storage is:", hashTable._storage);
+    // console.log("Is the hashTable._storage an array? "+Array.isArray(hashTable._storage));
+    // console.log("");
   });
+  describe("general", function(){
+    it("should have methods named 'insert' and 'retrieve", function() {
+      expect(hashTable.insert).toEqual(jasmine.any(Function));
+      expect(hashTable.retrieve).toEqual(jasmine.any(Function));
+    });
 
-  it("should have methods named 'insert' and 'retrieve", function() {
-    expect(hashTable.insert).toEqual(jasmine.any(Function));
-    expect(hashTable.retrieve).toEqual(jasmine.any(Function));
+    it("should handle hash function collisions", function(){
+      // force the hash function to return 0
+      spyOn(window, 'getIndexBelowMaxForKey').andReturn(0);
+      var v1 = 'val1', v2 = 'val2';
+      hashTable.insert(v1, v1);
+      hashTable.insert(v2, v2);
+      expect(hashTable.retrieve(v1)).toEqual(v1);
+      expect(hashTable.retrieve(v2)).toEqual(v2);
+    });
   });
-
-  it("should handle hash function collisions", function(){
-    // force the hash function to return 0
-    spyOn(window, 'getIndexBelowMaxForKey').andReturn(0);
-    var v1 = 'val1', v2 = 'val2';
-    hashTable.insert(v1, v1);
-    hashTable.insert(v2, v2);
-    expect(hashTable.retrieve(v1)).toEqual(v1);
-    expect(hashTable.retrieve(v2)).toEqual(v2);
-  });
-
   // add more tests!
+  describe("insert", function(){
+    it("should insert string key value pairs into the table", function(){
+      hashTable.insert("ken", "hungry");
+      expect(hashTable.retrieve("ken")).toEqual("hungry");
+    });
+
+    it("should handle collisions", function(){
+      hashTable._limit = 1;
+      hashTable.insert("ken", "hungry");
+      hashTable.insert("rohaus", "tired");
+      expect(hashTable.retrieve("ken")).toEqual("hungry");
+    });
+
+  });
+
+  describe("remove", function(){
+    it("should remove a key value pair from the hash table", function(){
+      hashTable.insert("ken", "hungry");
+      hashTable.remove("ken");
+      expect(hashTable.retrieve("ken")).toEqual(undefined);
+    });
+  });
+
+  describe("retrieve", function(){
+    it("should retrieve a value when the key is given", function(){
+      hashTable.insert("ken", "hungry");
+      expect(hashTable.retrieve("ken")).toEqual("hungry");
+    });
+  });
 });
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -12,20 +12,46 @@ var HashTable = function(){
   this._storage = makeLimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  console.log(i);
-  this._storage.set(i, v);
+HashTable.prototype.insert = function(key, value){
+  var i = getIndexBelowMaxForKey(key, this._limit);
+  // console.log(i);
+  var element = this._storage.get(i);
+  if (!element){
+    var newElement = [[key, value]];
+    this._storage.set(i, newElement);
+  }else{
+    // console.log("Is element an array?? " + Array.isArray(element));
+    // console.log("element is ", element);
+    element.push([key,value]);
+  }
 };
 
-HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
+HashTable.prototype.retrieve = function(key){
+  var i = getIndexBelowMaxForKey(key, this._limit);
+  console.log("Trying to retrieve...key: " + key);
+  console.log("this._storage.get(i) is: ", this._storage.get(i));
+  return this._storage.get(i)[0][1];
 };
 
-HashTable.prototype.remove = function(){
+HashTable.prototype.remove = function(key){
+  var i = getIndexBelowMaxForKey(key, this._limit);
+  this._storage.each(function(){
+    if ("If the array location contains the key, then delete it"){
+      this._storage.set(i, undefined);
+    }
+  });
 };
 
 // NOTE: For this code to work, you will NEED the code from hashTableHelpers.js
 // Start by loading those files up and playing with the functions it provides.
 // You don't need to understand how they work, only their interface is important to you
+
+// {
+//  set:(index, value) storage[index] = value;
+//  get:(index) return storage[index];
+//  each:(callback)
+//        for(var i = 0; i < storage.length; i++){
+//          callback(storage[i], i, storage);
+//        }
+// }
+// {set: fn, get: fn, each: fn, 0: [[key, value]]}
