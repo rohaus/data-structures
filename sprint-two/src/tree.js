1c361594726e082127be72a96a5fcbee90efd265
Hack Reactor Students <github@hackreactor.com>
2013-09-26 19:11:35 -0700

    Passes all tree tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -10,18 +10,32 @@ var makeTree = function(tree){
 var treeMethods = {};
 
 treeMethods.addChild = function(child){
-  this.children = [];
+  if (this.children === undefined){
+    this.children = [];
+  }
   this.children.push(makeTree(child));
 };
 
-treeMethods.contains = function(){
-
+treeMethods.contains = function(target){
+  var result = false;
+  if (this.value === target){
+    result = true;
+  }else if (this.children){
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].value === target){
+        result = true;
+      }else{
+        this.children[i].contains(target);
+      }
+    }
+  }
+  return result;
 };
 
 
 // tree = makeTree(); // {value: tree, children: undefined, contains: function(){} }
 // tree.addChild("ken"); // {value: tree, children: [{value: tree, children: "ken", contains: function(){}}], contains: function(){} }
-// tree.addChild("rohaus"); //
+// tree.addChild("rohaus"); // {value: tree, children: [{value: tree, children: "ken", contains: function(){}}, {value: tree, children: "rohaus", contains: function(){}}], contains: function(){} }
 
    //  O
    // o o
