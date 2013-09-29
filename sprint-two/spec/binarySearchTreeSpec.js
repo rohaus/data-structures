1aad6994497a97f3ddef034a1fc8a5edd86bb703
Hack Reactor Students <github@hackreactor.com>
2013-09-28 17:24:26 -0700

    Completes binary search tree

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -2,25 +2,141 @@ describe("binarySearchTree", function() {
   var binarySearchTree;
 
   beforeEach(function() {
-    binarySearchTree = makeBinarySearchTree();
+    binarySearchTree = makeBinarySearchTree(50);
   });
 
   it("should have methods named 'insert', 'contains', and 'depthFirstLog", function() {
-    expect(binarySearchTree.children).toEqual(jasmine.any(Function));
+    expect(binarySearchTree.insert).toEqual(jasmine.any(Function));
     expect(binarySearchTree.contains).toEqual(jasmine.any(Function));
     expect(binarySearchTree.depthFirstLog).toEqual(jasmine.any(Function));
   });
   // add more tests here to test the functionality of binarySearchTree
 
   describe("insert", function(){
-    
+    it("should not have left or right properties set to a value when nothing is inserted", function(){
+      expect(binarySearchTree.left).toEqual(undefined);
+      expect(binarySearchTree.right).toEqual(undefined);
+    });
+
+    it("should insert to the left when the value is lower than the parent node's value",function(){
+      binarySearchTree.insert(25);
+      expect(binarySearchTree.left.value).toEqual(25);
+    });
+
+    it("should insert to the right when the value is higher than the parent node's value",function(){
+      binarySearchTree.insert(75);
+      expect(binarySearchTree.right.value).toEqual(75);
+    });
+
+    it("should insert to the left when the value is lower than the parent node's left value",function(){
+      binarySearchTree.insert(25);
+      binarySearchTree.insert(10);
+      expect(binarySearchTree.left.left.value).toEqual(10);
+    });
+
+    it("should insert to the right when the value is higher than the parent node's left value",function(){
+      binarySearchTree.insert(25);
+      binarySearchTree.insert(40);
+      expect(binarySearchTree.left.right.value).toEqual(40);
+    });
+
+    it("should insert to the right when the value is higher than the parent node's value",function(){
+      binarySearchTree.insert(75);
+      expect(binarySearchTree.right.value).toEqual(75);
+    });
+
+    it("should insert to the left when the value is lower than the parent node's right value",function(){
+      binarySearchTree.insert(75);
+      binarySearchTree.insert(60);
+      expect(binarySearchTree.right.left.value).toEqual(60);
+    });
+
+    it("should insert to the right when the value is hight than the parent node's right value",function(){
+      binarySearchTree.insert(75);
+      binarySearchTree.insert(90);
+      expect(binarySearchTree.right.right.value).toEqual(90);
+    });
+
+    it("should not insert when the value is equal to the parent node's value",function(){
+      binarySearchTree.insert(50);
+      expect(binarySearchTree.left).toEqual(undefined);
+      expect(binarySearchTree.right).toEqual(undefined);
+    });
+
+    it("should not insert when the value is equal to an existing node",function(){
+      binarySearchTree.insert(25);
+      binarySearchTree.insert(75);
+      binarySearchTree.insert(25);
+      expect(binarySearchTree.left.left).toEqual(undefined);
+      expect(binarySearchTree.left.right).toEqual(undefined);
+      binarySearchTree.insert(75);
+      expect(binarySearchTree.right.left).toEqual(undefined);
+      expect(binarySearchTree.right.right).toEqual(undefined);
+    });
   });
 
   describe("contains", function(){
+    it("should return false if the value is not equal to the parent node's value",function(){
+      expect(binarySearchTree.contains(75)).toEqual(false);
+    });
 
+    it("should return true if the value is equal to the parent node's value",function(){
+      expect(binarySearchTree.contains(50)).toEqual(true);
+    });
+
+    it("should return false if the value is not in the tree which has two nodes",function(){
+      binarySearchTree.insert(25);
+      expect(binarySearchTree.contains(40)).toEqual(false);
+    });
+
+    it("should return true if the value is in the tree which has two nodes",function(){
+      binarySearchTree.insert(25);
+      expect(binarySearchTree.contains(25)).toEqual(true);
+    });
+
+    it("should return false if the value is not in the tree which has four nodes",function(){
+      binarySearchTree.insert(25);
+      binarySearchTree.insert(40);
+      binarySearchTree.insert(90);
+      expect(binarySearchTree.contains(75)).toEqual(false);
+    });
+
+    it("should return true if the value is in the tree which has four nodes",function(){
+      binarySearchTree.insert(25);
+      binarySearchTree.insert(40);
+      binarySearchTree.insert(90);
+      expect(binarySearchTree.contains(40)).toEqual(true);
+    });
   });
 
   describe("depthFirstLog", function(){
+    var callback = function(){
+      console.log("we went to this node: "+this.value);
+      this.value += 5;
+    };
 
+    it("should return the parent node's value in an array",function(){
+      binarySearchTree.depthFirstLog(callback);
+      expect(binarySearchTree.value).toEqual(55);
+    });
+
+    it("should return the all values in the tree in an array",function(){
+      binarySearchTree.insert(25);
+      binarySearchTree.depthFirstLog(callback);
+      expect(binarySearchTree.value).toEqual(55);
+      expect(binarySearchTree.left.value).toEqual(30);
+    });
+
+    it("should return the all values in the tree in an array",function(){
+      binarySearchTree.insert(25);
+      binarySearchTree.insert(40);
+      binarySearchTree.insert(75);
+      binarySearchTree.insert(90);
+      binarySearchTree.depthFirstLog(callback);
+      expect(binarySearchTree.left.value).toEqual(30);
+      expect(binarySearchTree.left.right.value).toEqual(45);
+      expect(binarySearchTree.right.value).toEqual(80);
+      expect(binarySearchTree.right.right.value).toEqual(95);
+    });
   });
 });
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,72 @@
-var makeBinarySearchTree = function(){
+var makeBinarySearchTree = function(value){
+  var tree = {};
+  tree.left = null; //an object (binary tree)
+  tree.right = null; //an object (binary tree)
+  tree.value = value; 
+  tree.insert = treeMethods.insert;
+  tree.contains = treeMethods.contains;
+  tree.depthFirstLog = treeMethods.depthFirstLog;
+  return tree;
 };
+
+var treeMethods = {};
+
+treeMethods.insert = function(value){
+  if(value < this.value){
+    if(this.left === null){
+      this.left = makeBinarySearchTree(value);
+    }else {
+      this.left.insert(value);
+    }
+  }else if(value > this.value){
+    if(this.right === null){
+      this.right = makeBinarySearchTree(value);
+    }else {
+      this.right.insert(value);
+    }
+  }
+};
+
+treeMethods.contains = function(value){
+  var result = result || false;
+  if(this.value === value){
+    result = true;
+  }else {
+    if(this.left !== null){
+      if(this.left.contains(value)){
+        result = true;
+      }
+    }
+    if(this.right !== null){
+      if(this.right.contains(value)){
+        result = true;
+      }
+    }
+  }
+  return result;
+};
+
+treeMethods.depthFirstLog = function(callback){
+  callback.call(this);
+
+  if(this.left !== null){
+    this.left.depthFirstLog(callback);
+  }
+  if(this.right !== null){
+    this.right.depthFirstLog(callback);
+  }
+
+  // var result = result || [];
+  // result.push(this.value);
+  // return result;
+
+
+
+};
+
+
+
+
+
+
+
